e82b93935b61fede44979da83d6692c6a7677a8f
Sabersamus <rcatron10@gmail.com>
2012-04-28 16:03:22 -0400

    added check to see if a player can place blocks inside a lot

--- src/info/bytecraft/zones/listeners/LotBuild.java
+++ src/info/bytecraft/zones/listeners/LotBuild.java
@@ -1,5 +1,7 @@
 package info.bytecraft.zones.listeners;
 
+import java.util.List;
+
 import info.bytecraft.zones.Zones;
 import info.bytecraft.zones.info.Lot;
 import info.bytecraft.zones.info.LotPlayers;
@@ -12,6 +14,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
 public class LotBuild implements Listener{
 	private final Zones plugin;
@@ -39,6 +42,38 @@ public class LotBuild implements Listener{
 								event.setCancelled(true);
 								player.sendMessage(ChatColor.RED + "You are not allowed to break blocks in the lot " + lot.getLotName() + " in the zone " + zone.getName());
 								player.setFireTicks(100);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onBuild(BlockPlaceEvent event){
+		Player player = event.getPlayer();
+		Location loc = event.getBlock().getLocation();
+		ZoneVector vector = new ZoneVector(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+		List<Zone> zones = plugin.getDatabase().find(Zone.class).where().ieq("worldName", loc.getWorld().getName()).findList();
+		for(Zone zone: zones){
+			if(zone.contains(vector)){
+				List<Lot> lots = plugin.getDatabase().find(Lot.class).where().ieq("zoneName", zone.getName()).findList();
+				for(Lot lot: lots){
+					if(lot.contains(vector)){
+						for(LotPlayers players: plugin.getDatabase().find(LotPlayers.class).where().ieq("zoneName", zone.getName()).ieq("lotName", lot.getLotName()).findList()){
+							if(players != null){
+								if(!player.getName().equalsIgnoreCase(players.getPlayerName())){
+									event.setCancelled(true);
+									player.sendMessage(ChatColor.RED + "You are not allowed to place blocks in the lot " + lot.getLotName() + " in the zone " + zone.getName());
+									player.setFireTicks(100);
+									return;
+								}
+							}else{
+								event.setCancelled(true);
+								player.sendMessage(ChatColor.RED + "You are not allowed to place blocks in the lot " + lot.getLotName() + " in the zone " + zone.getName());
+								player.setFireTicks(100);
+								return;
 							}
 						}
 					}
