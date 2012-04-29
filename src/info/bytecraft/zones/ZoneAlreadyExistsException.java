9ac1154e940213263488ab17bb20e2c6234febad
Sabersamus <rcatron10@gmail.com>
2012-04-28 20:59:50 -0400

    Added what zone threw ZoneAlreadyExistsException.

--- src/info/bytecraft/zones/ZoneAlreadyExistsException.java
+++ src/info/bytecraft/zones/ZoneAlreadyExistsException.java
@@ -1,7 +1,10 @@
 package info.bytecraft.zones;
 
+import info.bytecraft.zones.info.Zone;
+
 @SuppressWarnings("serial")
 public class ZoneAlreadyExistsException extends Exception{
+	private static Zone zone;
 	
 	public ZoneAlreadyExistsException(String message){
 		super(message);
@@ -9,6 +12,15 @@ public class ZoneAlreadyExistsException extends Exception{
 	
 	public ZoneAlreadyExistsException(String message, Throwable ex){
 		super(message,ex);
+	}
+	
+	public ZoneAlreadyExistsException(String message, Zone zone){
+		super(message);
+		ZoneAlreadyExistsException.zone = zone;
+	}
+	
+	public static Zone getZone(){
+		return zone;
 	}
 	
 }
